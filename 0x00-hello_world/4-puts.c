07551f688baa2d9f40b20eb4af7860933eba2242
Ademola <sikirusulaimon2002@gmail.com>
2022-04-08 01:56:03 -0700

    adjusting task 04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 include <stdio.h>
 
-
-
 /**
  *
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
@@ -9,13 +7,8 @@ include <stdio.h>
  *   * Return: 0 if exited properly, non-zero otherwise
  *
  *    */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
