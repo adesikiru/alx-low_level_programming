06d30d05c86ed4e833063cefd44f3c3951c263cb
Ademola <sikirusulaimon2002@gmail.com>
2022-04-09 05:25:26 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i, b, c, d;
+
+	for (i = 0; i < 1000; i++)
+	{
+		b = i / 100; /* hundreds */
+		c = (i / 10 )% 10 ; /* tens */
+		d = i % 10; /* singles */
+
+		if (b < c && c < d)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
