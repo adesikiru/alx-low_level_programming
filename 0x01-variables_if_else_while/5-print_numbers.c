8cc00fc08fc8623a2fc17ad075642f41dd6b19c1
Ademola <sikirusulaimon2002@gmail.com>
2022-04-08 18:23:03 -0700

    task 06

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Descrption: Use main function to printsingle digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
