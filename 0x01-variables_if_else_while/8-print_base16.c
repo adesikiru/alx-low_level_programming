20b6f2f1b196c7db4bf368ee746debc1253253a9
Ademola <sikirusulaimon2002@gmail.com>
2022-04-08 18:45:45 -0700

    task 08

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print all numbers of base64 in lowercase.
+ * You can only use `putchar`
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
