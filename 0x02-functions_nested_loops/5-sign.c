e2934ea140783b97d20075f020cd1ebf137eb4f8
Ademola <sikirusulaimon2002@gmail.com>
2022-04-12 16:40:44 -0700

    task 05

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - Entry Point
+ *
+ * Description: a function that prints
+ * the sign of a number.
+ *
+ * @n: the int to print
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
