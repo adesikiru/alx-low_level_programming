6c9883a3cc5dd8cdfa49f19154982bb0b1d7ef91
Ademola <sikirusulaimon2002@gmail.com>
2022-04-12 15:58:46 -0700

    task 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Entry Point
+ *
+ * Description: function that prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+
+	int ch, counter = 0;
+
+	while (counter < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		counter++;
+		_putchar('\n');
+	}
+
+}
