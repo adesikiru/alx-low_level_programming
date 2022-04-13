987b61ef291c18e704501a9ed2e464f28fd62321
Sikiru Ademola <77970450+Ade2002@users.noreply.github.com>
2022-04-13 21:49:32 +0100

    Update 0-main.c
    adjust task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
-#include <stdio.h>
-
 /**
- * main - Test function for positive or negative
- * Description:  a test for a
- * postitive_or_negative()
+ * main - entry point
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }
