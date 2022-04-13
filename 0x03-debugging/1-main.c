965e18aea1bf2c3c423db7690948d8e33b225aec
Ademola <sikirusulaimon2002@gmail.com>
2022-04-13 13:31:50 -0700

    task 01

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Description: remove lines of code
+ * that causes infite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+int 1;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("Infinite loop avoided! \\o/\n");
+
+return(0);
+}
