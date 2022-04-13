1132e86313aa90a5388e24e1ee51f5ff945f053e
Ademola <sikirusulaimon2002@gmail.com>
2022-04-13 04:22:50 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Entry Point
+ *
+ * Description: a function that adds two
+ * integers and returns the result.
+ *
+ * @a: print int a
+ * 
+ * @b: print int b
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
