d7e511d9d7612e11fa4b58bb610f21254dadf19f
Ade2002 <sikirusulaimon2002@gmail.com>
2022-07-13 00:53:37 +0100

    ade

--- 0x18-dynamic_libraries/dummyfunction.c
+++ 0x18-dynamic_libraries/dummyfunction.c
@@ -3,31 +3,38 @@ int _putchar(char c)
 
 }
 
-int _islower(int c){
+int _islower(int c)
+{
 
 }
 
-int _isalpha(int c){
+int _isalpha(int c)
+{
 
 }
 
-int _abs(int n){
+int _abs(int n)
+{
 
 }
 
-int _isupper(int c){
+int _isupper(int c)
+{
 
 }
 
-int _isdigit(int c){
+int _isdigit(int c)
+{
 
 }
 
-int _strlen(char *s){
+int _strlen(char *s)
+{
 
 }
 
-void _puts(char *s){
+void _puts(char *s)
+{
 
 }
 
